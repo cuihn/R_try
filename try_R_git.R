7a244c998743b366bfb46617247b54b526dc6ec5
Haining Cui <cuihn1989@gmail.com>
2023-11-14 20:00:12 -0500

    add

--- try_R_git.R
+++ try_R_git.R
@@ -1,9 +1,17 @@
 # Haining is tying
 
-# library(tidyverse)
-#library(palmerpenguins)
+library(tidyverse)
+library(palmerpenguins)
 
 penguins %>%
   ggplot(aes(x = bill_depth_mm)) +
   geom_histogram()
-# synced to gitihut for testing
+# synced to githut for testing 11/14
+
+
+#
+
+
+
+
+EEEESSS
