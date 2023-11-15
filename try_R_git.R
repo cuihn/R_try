602e21feca2170e190345bdece4783ae38d601f2
Haining Cui <cuihn1989@gmail.com>
2023-11-14 19:08:08 -0500

    for last testing_Rstudio_sync_github

--- try_R_git.R
+++ try_R_git.R
@@ -6,14 +6,4 @@
 penguins %>%
   ggplot(aes(x = bill_depth_mm)) +
   geom_histogram()
-<<<<<<< HEAD
-
-=======
-# hello I am trying to pull files
->>>>>>> f5552075262af574f2ba1cacf18bd96683f2e830
-
-
-penguins %>%
-  ggplot(aes(x = bill_depth_mm)) +
-  geom_histogram()
-<<<<<<< HEAD
+# synced to gitihut for testing
